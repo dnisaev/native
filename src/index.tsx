b8c3107bcb1597b65683daece60ff5aa0a7a48af
dnisaev <dnisaev@yandex.ru>
2023-11-19 16:23:34 +0300

    update repo native

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// import {splitIntoWords} from "./01-hello-tests/01";
 
+// const sentense = "hello my friend";
+// const result = splitIntoWords(sentense);
+// console.log(result[0] === "hello");
+// console.log(result[1] === "my");
+// console.log(result[2] === "friend");
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
